f0c9e5a336df15ce5aa4d1c45c2e0b5f00d45798
Maheshwara  Sampath <83503593+sampath24-ss@users.noreply.github.com>
2025-03-29 17:09:18 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws"{
+region = "ap-south-1"
+}
+
+resource "aws_instance" "one"{
+count = 2
+ami = "ami-0c9361875f5ad032c"
+instance_type = "t2.micro"
+key_name = "NEW_VALUE"
+tags ={
+Name = var.instance_names[count.index]
+}
+}
+
+variable "instance_names" {
+default = ["jenkins", "nexus" , "Appserver-1", "Appserver-2","Monitoring server"]
+}
